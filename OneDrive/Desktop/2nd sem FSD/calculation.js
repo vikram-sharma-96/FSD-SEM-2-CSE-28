93e0cc8d5b36d20b608487ce2645639de95c32c0
vikram-sharma-96 <vikram.25b01010075@abes.ac.in>
2026-02-28 23:02:25 +0530

    calculation in java

---
+++ OneDrive/Desktop/2nd sem FSD/calculation.js
@@ -0,0 +1,6 @@
+let a=5;
+let b=6;
+console.log("sum of a and b", a+b);
+console.log("substraction of a and b",a-b);
+console.log("multiplication of a and b",a*b);
+console.log("division of a and b",a/b);
