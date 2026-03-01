0f58ad40c5103c6819f02e580e3d4b379cbad7a7
vikram-sharma-96 <vikram.25b01010075@abes.ac.in>
2026-03-01 09:04:36 +0530

    js questions

---
+++ OneDrive/Desktop/2nd sem FSD/js question practice/Q1.js
@@ -0,0 +1,13 @@
+// write a code  which can give grades to students
+let score=75;
+let grade;
+if(score>90 && score<=100)
+    console.log("he got A grade");
+else if(score>75 && score<=90)
+    console.log("he got B grade");
+else if(score>60 && score<=75)
+    console.log("he got c grade");
+else if(score>45 && score<=60)
+    console.log("he got d grade");
+else
+    console.log("he got f grade and need improvement");
