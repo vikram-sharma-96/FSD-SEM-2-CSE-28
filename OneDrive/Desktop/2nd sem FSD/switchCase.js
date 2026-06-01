7033bcc507560c8fb7ebb88198bd77c6e560ffb6
vikram-sharma-96 <vikram.25b01010075@abes.ac.in>
2026-06-01 12:41:50 +0530

    switch cases

---
+++ OneDrive/Desktop/2nd sem FSD/switchCase.js
@@ -0,0 +1,12 @@
+let day = 3;
+switch(day){
+    case 1:
+        console.log("monday")
+        break;
+     case 2:
+    console.log("Tuesday")
+         break;
+    case 3:
+        console.log("Thursday")
+        break;
+}
